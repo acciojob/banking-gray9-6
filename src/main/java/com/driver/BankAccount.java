6ebae4df6cbd156b2ffd285e4ce1e369a533ac78
Abhay Kumar <kabhay166@gmail.com>
2023-06-17 15:29:50 +0530

    "First Push"

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -1,31 +1,82 @@
 package com.driver;
 
 public class BankAccount {
-
     private String name;
     private double balance;
     private double minBalance;
 
-    public BankAccount(String name, double balance, double minBalance) {
+//    private boolean isAccountNumberGenerated;
 
+    public BankAccount(String name, double balance, double minBalance) {
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
+
+//        this.isAccountNumberGenerated = false;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
+         int temp = digits;
+         int digitSum = 0;
+         while(temp > 0){
+             int digit = temp%10;
+             digitSum += digit;
+             temp = temp/10;
+         }
 
-        return null;
+         if(digitSum == sum){
+//             this.isAccountNumberGenerated = true;
+             return Integer.toString(digits);
+         } else {
+             throw new RuntimeException("Account Number can not be generated");
+         }
     }
 
     public void deposit(double amount) {
         //add amount to balance
-
+            this.balance += amount;
+//        System.out.println(this.balance);
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+        if(this.balance < amount){
+            throw new RuntimeException("Insufficient Balance");
+        }else if (amount < this.balance ){
+            this.balance -= amount;
+            System.out.println(this.balance);
+        }
+        if(this.balance < this.minBalance){
+            throw new RuntimeException("Insufficient Balance");
+        }
     }
 
+
+    // getter and Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
+    }
 }
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -5,15 +5,64 @@ public class CurrentAccount extends BankAccount{
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-
+        super(name,balance,5000);
+        if(balance < 5000){
+            throw new RuntimeException("Insufficient Balance");
+        }
+        this.tradeLicenseId = tradeLicenseId;
     }
 
-    public void validateLicenseId() throws Exception {
-        // A trade license Id is said to be valid if no two consecutive characters are same
-        // If the license Id is valid, do nothing
-        // If the characters of the license Id can be rearranged to create any valid license Id
-        // If it is not possible, throw "Valid License can not be generated" Exception
 
+    public void validateLicenseId() throws Exception {
+
+        // Check if the license ID is already valid
+        boolean isValid = true;
+        for (int i = 0; i < tradeLicenseId.length() - 1; i++) {
+            if (tradeLicenseId.charAt(i) == tradeLicenseId.charAt(i + 1)) {
+                isValid = false;
+                break;
+            }
+        }
+
+        if (!isValid) {
+            // Try rearranging the characters to create a valid license ID
+            char[] chars = tradeLicenseId.toCharArray();
+            for (int i = 0; i < chars.length - 1; i++) {
+                if (chars[i] == chars[i + 1]) {
+                    for (int j = 0; j < chars.length; j++) {
+                        if (chars[j] != chars[i] && (i == 0 || chars[j] != chars[i - 1])) {
+                            // Swap the characters at positions i and j
+                            char temp = chars[i];
+                            chars[i] = chars[j];
+                            chars[j] = temp;
+
+                            // Check if the rearranged license ID is valid
+                            isValid = true;
+                            for (int k = 0; k < chars.length - 1; k++) {
+                                if (chars[k] == chars[k + 1]) {
+                                    isValid = false;
+                                    break;
+                                }
+                            }
+
+                            if (isValid) {
+                                tradeLicenseId = new String(chars);
+                                return;
+                            }
+                        }
+                    }
+
+                    // If no valid rearrangement is possible, throw an exception
+                    throw new RuntimeException("Valid License can not be generated");
+                }
+            }
+        }
+    }
+
+    private void rearrangeLicenseIdChars(char[] chars, int index) {
+        char temp = chars[index];
+        chars[index] = chars[index - 1];
+        chars[index - 1] = temp;
     }
 
 }
