6fa8703c7232e023e177381e8fcd7897886dbf86
Abhay Kumar <kabhay166@gmail.com>
2023-06-17 16:15:19 +0530

    "Third Push"

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -5,7 +5,9 @@ public class StudentAccount extends BankAccount{
     String  institutionName;
 
     public StudentAccount(String name, double balance, String  institutionName) {
-        //minimum balance is 0 by default
+        //minimum balance is 0 by default public void setInstitutionName(String institutionName) {
+        //        this.institutionName = institutionName;
+        //    }
         super(name,balance,0);
         this.institutionName = institutionName;
 
@@ -15,7 +17,5 @@ public class StudentAccount extends BankAccount{
         return institutionName;
     }
 
-    public void setInstitutionName(String institutionName) {
-        this.institutionName = institutionName;
-    }
+
 }
